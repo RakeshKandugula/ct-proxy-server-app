2a5b6ed8685a06c44718752afdea11cd9a187744
Maksims Klimjuks <Maksims.Klimjuks@stockmann.com>
2025-02-18 11:57:43 +0200

    Update constants.js
    Changes Yes/No values to start with capital letter

--- src/Components/constants.js
+++ src/Components/constants.js
@@ -214,8 +214,8 @@ export const ticketTypes = [
 
 export const poLocations = ["Distribution Centre B&M", "Distribution Centre DR warehouse", "Helsinki Departmental Store", "Itis Departmental Store", "Jumbo Departmental Store", "Riga Departmental Store", "Tallinn Departmental Store", "Tampere Departmental Store", "Tapiola Departmental Store", "Turku Departmental Store"];
 export const poTypes = ["PRE","AdHoc", "CD", "LC"];
-export const poEDIs = ["yes", "no"];
-export const orderPriceTags = ["yes", "no"];
+export const poEDIs = ["Yes", "No"];
+export const orderPriceTags = ["Yes", "No"];
 
 export const lifestyleDetails = {
   Womens: [
