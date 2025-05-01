d4d85690917439ecc33cce2be56cf425c4e30187
ShashankaSukkala <Shashanka.Sukkala@stockmann.com>
2025-05-01 15:13:21 +0300

    Update constants.js
    Removed Mayoral

--- src/Components/constants.js
+++ src/Components/constants.js
@@ -60,8 +60,7 @@ export const suppliers = [
   { value: "TIGER_OF_SWEDEN_FINLAND_OY", label: "TIGER_OF_SWEDEN_FINLAND_OY" },
   { value: "BIRKENSTOCK_GMBH_&_KG", label: "BIRKENSTOCK_GMBH_&_KG" },
   { value: "OSCAR_JACOBSON_AB", label: "OSCAR_JACOBSON_AB" },
-  { value: "OPUS_GMBH", label: "OPUS_GMBH" },
-  { value: "MAYORAL_MODA_INFANTIL_SAU", label: "MAYORAL_MODA_INFANTIL_SAU" }
+  { value: "OPUS_GMBH", label: "OPUS_GMBH" }
 ];
 
 
