4119c85366749d992045da493d8dc934c3da1d46
ShashankaSukkala <Shashanka.Sukkala@stockmann.com>
2025-08-28 07:51:53 +0300

    Update excelToXml.js
    GSL_AB

--- src/Components/excelToXml.js
+++ src/Components/excelToXml.js
@@ -171,9 +171,13 @@ function convert(
       const sheet = workbook.Sheets[sheetName];
       const fullRange = XLSX.utils.decode_range(sheet['!ref']);
       fullRange.s.r=findHeaderRow(sheet);
-      console.log(fullRange.s);
      data = XLSX.utils.sheet_to_json(sheet, { header: 1, range: fullRange });
-    }
+    }else if(supplierName==="GSL_AB"){
+      const sheet = workbook.Sheets[sheetName];
+      const fullRange = XLSX.utils.decode_range(sheet['!ref']);
+      fullRange.s.r=findHeaderRow(sheet);
+     data = XLSX.utils.sheet_to_json(sheet, { header: 1, range: fullRange });
+  }
   else {
     data = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], { header: 1 });
   }
