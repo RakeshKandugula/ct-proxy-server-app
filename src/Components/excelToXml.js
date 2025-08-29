496ca7e10fc017488e724d286167550cde1a69a8
ShashankaSukkala <Shashanka.Sukkala@stockmann.com>
2025-08-29 14:32:33 +0300

    Update excelToXml.js
    added CCDK COPENHAGEN

--- src/Components/excelToXml.js
+++ src/Components/excelToXml.js
@@ -192,6 +192,11 @@ function convert(
       const fullRange = XLSX.utils.decode_range(sheet['!ref']);
       fullRange.s.r=findHeaderRow(sheet);
      data = XLSX.utils.sheet_to_json(sheet, { header: 1, range: fullRange });
+    }else if(supplierName==="ONLINE_TEXTILE_SOLUTIONS_A/S"){
+      const sheet = workbook.Sheets[sheetName];
+      const fullRange = XLSX.utils.decode_range(sheet['!ref']);
+      fullRange.s.r=findHeaderRow(sheet);
+     data = XLSX.utils.sheet_to_json(sheet, { header: 1, range: fullRange });
     }
   else {
     data = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], { header: 1 });
