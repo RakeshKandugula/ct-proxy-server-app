786732da56a7190437fb78c78b37a53c00ef1671
ShashankaSukkala <Shashanka.Sukkala@stockmann.com>
2025-09-08 16:38:12 +0300

    Update excelToXml.js
    ROHNISCH_SPORTSWEAR_AB ADDED

--- src/Components/excelToXml.js
+++ src/Components/excelToXml.js
@@ -204,8 +204,15 @@ function convert(
       fullRange.s.r=findHeaderRow(sheet);
       //console.log(fullRange.s+"--"+supplierName);
      data = XLSX.utils.sheet_to_json(sheet, { header: 1, range: fullRange });
-    }
+    }else if(supplierName==="ROHNISCH_SPORTSWEAR_AB"){
+      const sheet = workbook.Sheets[sheetName];
+      const fullRange = XLSX.utils.decode_range(sheet['!ref']);
+      fullRange.s.r=findHeaderRow(sheet);
+      //console.log(fullRange.s+"--"+supplierName);
+     data = XLSX.utils.sheet_to_json(sheet, { header: 1, range: fullRange });
+  }
   else {
+    
     data = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], { header: 1 });
   }
   
