ddb48c4090bf3153339f2a5dcba13008fd461929
ShashankaSukkala <Shashanka.Sukkala@stockmann.com>
2025-09-08 11:44:42 +0300

    Update excelToXml.js
    Cinzia rocca

--- src/Components/excelToXml.js
+++ src/Components/excelToXml.js
@@ -198,6 +198,12 @@ function convert(
       fullRange.s.r=findHeaderRow(sheet);
       console.log(fullRange.s+"--"+supplierName);
      data = XLSX.utils.sheet_to_json(sheet, { header: 1, range: fullRange });
+    }else if(supplierName==="RODEL_S.P.A."){
+      const sheet = workbook.Sheets[sheetName];
+      const fullRange = XLSX.utils.decode_range(sheet['!ref']);
+      fullRange.s.r=findHeaderRow(sheet);
+      //console.log(fullRange.s+"--"+supplierName);
+     data = XLSX.utils.sheet_to_json(sheet, { header: 1, range: fullRange });
     }
   else {
     data = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], { header: 1 });
